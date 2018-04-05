3b686fc3a4d7e0260a6b792e2ab1b494cd070a38
Mohamed Ashraf <mohammed.ashraaf.15@gmail.com>
2018-04-06 00:50:36 +0200

    Update PreProssing.py

--- PreProssing.py
+++ PreProssing.py
@@ -4,7 +4,6 @@ import imageio
 
 
 def resize_images(folder_read, folder_save, size=(100, 100)):
-
     open_training_files = glob.glob('{}/*.jpg'.format(folder_read))
     save_training_files = map(lambda x: x.split('/')[1].split('.')[0], open_training_files)
     for file_open, file_save in zip(open_training_files, save_training_files):
