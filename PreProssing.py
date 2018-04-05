a4ed2839128f496158e8c4d70e13849092a5d846
= <mohammed.ashraaf.15@gmail.com>
2018-04-05 23:07:01 +0200

    Resize the image with the same name

--- PreProssing.py
+++ PreProssing.py
@@ -1,15 +1,8 @@
 from PIL import Image
-import numpy as np
 import glob
-import cv2
 
-i = 0
 for filename in glob.glob('train/*.jpg'):
-    img=Image.open(filename)
+    img = Image.open(filename)
+    filename = filename.split('/')[1].split('.')[0]
     img = img.resize((100, 100), Image.ANTIALIAS)
-    img.save('Small-Img/'+str(i)+'.jpg')
-    i+=1
-
-
-#pixels = x_data.flatten().reshape(1, 12288)
-
+    img.save('small/'+filename+'.jpg')
