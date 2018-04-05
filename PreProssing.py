ff53a78b5433e3a150a744b2eadb7e332a685412
= <mohammed.ashraaf.15@gmail.com>
2018-04-06 00:47:46 +0200

    Create dynamic resizing function & allow loading image as vector

--- PreProssing.py
+++ PreProssing.py
@@ -1,8 +1,17 @@
 from PIL import Image
 import glob
+import imageio
 
-for filename in glob.glob('train/*.jpg'):
-    img = Image.open(filename)
-    filename = filename.split('/')[1].split('.')[0]
-    img = img.resize((100, 100), Image.ANTIALIAS)
-    img.save('small/'+filename+'.jpg')
+
+def resize_images(folder_read, folder_save, size=(100, 100)):
+
+    open_training_files = glob.glob('{}/*.jpg'.format(folder_read))
+    save_training_files = map(lambda x: x.split('/')[1].split('.')[0], open_training_files)
+    for file_open, file_save in zip(open_training_files, save_training_files):
+        img = Image.open(file_open)
+        img = img.resize(size, Image.ANTIALIAS)
+        img.save('{}/{}.jpg'.format(folder_save, file_save))
+
+
+def load_image_as_vector(img_path):
+    return imageio.imread(img_path).flatten()
